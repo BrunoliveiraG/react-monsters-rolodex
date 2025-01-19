cc7a94bb4cace69f5aca872dd53c3364ebcb14c1
brunoliveirag <brunoliveiragomesz@gmail.com>
2025-01-19 14:32:05 -0300

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-monsters-rolodex/', // Base URL for GitHub Pages deployment
   plugins: [react()],
   test: {
     globals: true,
